175745c85e5aa051e4af12360201c1d2ec374580
Cybercena <78sonamtmz@gmail.com>
2024-05-18 15:04:32 +0545

    string as array

---
+++ learning/17StringArray.py
@@ -0,0 +1,22 @@
+#Illustrate string as array
+a = "this is demo"
+print(a[0])
+print("\n")
+#looping through string
+for x in "Apple":
+    print(x)
+print("\n")
+
+#string length
+a = "hello sathi"
+print(len(a))
+
+##checking string 
+
+txt = "My country Nepal"
+print ("Nepal" in txt)
+print(txt)
+
+###check if not
+text = "this is demo"
+print("draft" not in text)
