55500592a5e6aad208c2dcd3a8bbde4d569a33c2
Cybercena <78sonamtmz@gmail.com>
2024-05-19 22:52:09 +0545

    Escape character

---
+++ learning/20_StringEscape.py
@@ -0,0 +1,24 @@
+#Using escape characters to insert illegal strings and character we use Escape character
+#Using double qoute inside the string
+txt = "We are so called \"Gorkhali\""
+print (txt)
+
+#using single qoute inside the string
+print("I love reading novel \'MunaMadan\'")
+
+#using backslash
+print("this is backslash \\")
+
+
+#Printing in new line 
+print ("1\n 2\n 3\n")
+
+#Carriage return
+txt = "this\ra"
+print(txt) 
+
+#using tab
+print("this\tis\tab")
+
+#using backspace:used to delete one characcter backspace
+print("this \bis\bbackspace")
