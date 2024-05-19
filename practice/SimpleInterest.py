b9d10b283a8566667b69d354e3802f7fe6d652ea
Sonam Tamang <102862975+Cybercena@users.noreply.github.com>
2024-05-19 13:45:44 +0545

    Adding practice problem

---
+++ practice/SimpleInterest.py
@@ -0,0 +1,6 @@
+#program to take P,T,R from user and find out simple interest,if P,T and R are missing then there value will be 1000 , 12 and 10% respectively 
+def SimpleInterest(p=1000,t=12,r=0.1):
+    print(p*t*r/100)
+SimpleInterest ()
+SimpleInterest (1000,24,0.1)
+SimpleInterest (t=12,r=0.2) 
---
+++ practice/factorial.py
@@ -0,0 +1,9 @@
+def factorial(*num):
+    #using for loop to take one by one integer
+    for value in num:
+        fact=1
+        for i in  range(1,value+1):
+            fact=fact*i
+        print(fact)
+factorial(1,2,4,5)        
+        
