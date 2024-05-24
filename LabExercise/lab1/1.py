8f7800e6de9ad9f3a7f78a121309ff391c478b49
Cybercena <78sonamtmz@gmail.com>
2024-05-24 21:48:15 +0545

    added file

---
+++ LabExercise/lab1/1.py
@@ -0,0 +1,10 @@
+#Wap to read two numbers (say x and y ) and perform following operations:
+# a.Display binary equivalent of the numbers
+# b.Perform bitwise AND, OR and XOR operations and display their results in binary format
+# c.Perform once complement of x and display the result in binary format
+# d.Left shift the x by 3 positions and display the result in binary format
+# e.Left shift the y by 2 positions and display the result in binary format
+
+x = int(input("Enter the value of x: "))
+y = int(input("Enter the value of y: "))
+print(f"The binary value of x and y is {bin(x)} and {bin(y)} respectively.")
