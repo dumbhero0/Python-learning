47999da8d8dba07533bc6f4814135b4e46320b3e
cybercena <78sonamtmz@gmail.com>
2024-06-25 22:54:55 +0545

    modified

--- LabExercise/lab1/5.py
+++ LabExercise/lab1/5.py
@@ -1,17 +1,25 @@
 #WAP that reads a multi-sentence string from user. Separate each sentence in the string and display each sentence. Again separate each word in the string and display then. Comma should not be included in the words.
 
-str=[]
-print("Enter the multiline string, if you want to stop writing enter \"00\": ")
-while(str!="00"):
-    user_input= input()
+# Initialize an empty list to hold the input strings
+str_list = []
+print('Enter the multi-line string. If you want to stop writing, enter "00": ')
+
+# Read input from the user until "00" is entered
+while True:
+    user_input = input()
     if user_input == "00":
         break
-    str.append(user_input)
-print(str)
-    
-for line in str:
+    str_list.append(user_input)
+
+# Display each sentence
+print("\nSentences:")
+for line in str_list:
     print(line)
-print("\n\n")
-for line in str:
-    for word in line:
+
+print("\nWords:")
+# Separate each word in the string and display them without commas
+for line in str_list:
+    # Split the line into words, removing commas
+    words = line.replace(",", "").split()
+    for word in words:
         print(word)
